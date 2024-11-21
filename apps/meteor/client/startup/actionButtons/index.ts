75eeaec4d3b19b67481b033a92e2573acb581b1a
MartinSchoeler <martinschoeler8@gmail.com>
2024-11-21 15:53:53 -0300

    chore: remove meteor.startup from permalink-pinned

--- apps/meteor/client/startup/actionButtons/index.ts
+++ apps/meteor/client/startup/actionButtons/index.ts
@@ -2,7 +2,6 @@ import './jumpToMessage';
 import './jumpToPinMessage';
 import './jumpToSearchMessage';
 import './jumpToStarMessage';
-import './permalinkPinned';
 import './permalinkStar';
 import './pinMessage';
 import './starMessage';
